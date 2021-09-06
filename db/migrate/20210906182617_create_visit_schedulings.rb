4c9592c1e4bcb599c2330a7e7b7ec0da4469c710
KassioScarpati <kassio.scarpati@gmail.com>
2021-09-06 15:38:01 -0300

    feat: add model visit scheduling

---
+++ db/migrate/20210906182617_create_visit_schedulings.rb
@@ -0,0 +1,11 @@
+class CreateVisitSchedulings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :visit_schedulings do |t|
+      t.datetime :date
+      t.string :status
+      t.references :contact, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/visit_scheduling_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class VisitSchedulingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
