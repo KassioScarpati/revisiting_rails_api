7634e3e3860b15448f00259f434aa6f6001520ba
Kassio Scarpati <kassio.scarpati@gmail.com>
2024-12-25 16:55:21 -0300

    refactor: frontend features removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,9 +12,6 @@ Rails.application.configure do
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
-  # Turn on fragment caching in view templates.
-  config.action_controller.perform_caching = true
-
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
